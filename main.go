e1628fb2ba46b0dcc814366f58962b0732152ee8
wu <664065745@qq.com>
2025-10-14 00:13:07 +0800

    chore(main): 移除未使用的数据库配置注释- 删除了 PostgreSQL 和 MongoDB 的配置注释 - 清理了 main.go 文件中的冗余代码 -保留了 HTTP mock handler 的核心逻辑

--- main.go
+++ main.go
@@ -7,9 +7,3 @@ func main() {
 	httpHandler := http_mock.NewHttpMockHandler(":8080", "D:\\code\\mock-go\\http.json")
 	httpHandler.Start()
 }
-
-//	pgDSN = "postgres://gj:xbrother123@localhost:5432/benchmark_db?sslmode=disable"
-//
-//	// MongoDB 配置
-//	mongoURI      = "mongodb://gj:xbrother123@localhost:27017"
-//	mongoDatabase = "benchmark_db"
