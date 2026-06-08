6601bd6c4782c5d0d9cb7a526113a24dcbcac18a
wutree <664065745@qq.com>
2026-06-08 15:53:12 +0800

    feat(record):sql

--- bin/sql.sql
+++ bin/sql.sql
@@ -3,5 +3,7 @@ WHERE source NOT IN (SELECT id FROM vertex)
    OR target NOT IN (SELECT id FROM vertex);
 sk-6529b87692e4b342d8838947fab936a05a8d9a86dea71427da0b9902aa69a4a0
 
+  http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/e8a5a9b1-ba5d-42d0-aa0b-bbedd74789e1?P1=1780916557&P2=404&P3=2&P4=cV19hxjaaGnPHYkSE99gHZ7bcoUHo8z7ts6qOQRdpKeB8LfQ%2bPOVlx1%2bf3mow2yfYYJedr%2bKmQj%2bkXmEa%2biFPg%3d%3d
+
 {"parent":"project_root","scene":{"name":"0_1212","renderDsl":{"@type":"type.googleapis.com/xtwin.api.type.JSONContent","json":{"cfg":{},"group":{"row":[1],"col":[1],"list":[{"x1":0,"y1":0,"x2":1,"y2":1,"key":"XB000","view":"XB000"}]},"view":{"XB000":{"key":"XB000","layout":"XB000","cfg":{"customLayoutStyle":{"theme":"def_UE_schemalayout_theme_1"}}}},"instance":{"XB000":{"type":"HTPage","cfg":{"rid":"project_root","ciType":"1"}}},"layout":{"XB000":{"row":[1],"col":[1],"list":[{"x1":0,"y1":0,"x2":1,"y2":1,"key":"XB000","cfg":{}}]}}},"en_json":{"cfg":{},"group":{"row":[1],"col":[1],"list":[{"x1":0,"y1":0,"x2":1,"y2":1,"key":"XB000","view":"XB000"}]},"view":{"XB000":{"key":"XB000","layout":"XB000","cfg":{"customLayoutStyle":{"theme":"def_UE_schemalayout_theme_1"}}}},"instance":{"XB000":{"type":"HTPage","cfg":{"rid":"project_root","ciType":"1"}}},"layout":{"XB000":{"row":[1],"col":[1],"list":[{"x1":0,"y1":0,"x2":1,"y2":1,"key":"XB000","cfg":{}}]}}}},"displayName":"-"}}
 http://193.3.62.200/xtwin/v3/metaverse/v1/scenes
